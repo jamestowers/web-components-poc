0ae7525dbb4c88761f439ff93d92ebaa3f621edf
James <Towers>
2021-06-23 15:43:08 +0100

    Add component library react wrapper

---
+++ wc-mono/il-wc-react/src/components.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'il-wc';
+
+import { defineCustomElements } from 'il-wc/loader';
+
+defineCustomElements();
+export const IlField = /*@__PURE__*/createReactComponent<JSX.IlField, HTMLIlFieldElement>('il-field');
+export const IlForm = /*@__PURE__*/createReactComponent<JSX.IlForm, HTMLIlFormElement>('il-form');
+export const IlNotice = /*@__PURE__*/createReactComponent<JSX.IlNotice, HTMLIlNoticeElement>('il-notice');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
